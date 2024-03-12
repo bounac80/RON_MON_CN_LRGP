eb4aadbe75ba8acf97f433055c58234b7d0240c7
bounac80 <132282485+bounac80@users.noreply.github.com>
2024-03-12 16:20:54 +0100

    Update RON_MON_CN_Project_LRGP.py

--- RON_MON_CN_Project_LRGP.py
+++ RON_MON_CN_Project_LRGP.py
@@ -158,9 +158,12 @@ st.write('number of Nitrogen atom-  N: ', D2[5])
 st.write('exact molecular weight [g/mol]:', D1['MW'])
 
 st.write('all the descriptors (for more information about descriptiors see [documentation](http://mordred-descriptor.github.io/documentation/master/descriptors.html))', D1)
-st.dataframe(D1)
-st.write('exact molecular weight [g/mol]:', D1['ABC'])
-
+#st.dataframe(D1)
+st.write('111111111111111111:', D1['ABC'])
+st.write('222222222222222222:', D1['ABC'][0])
+dede=3.2
+dede=D1['ABC'][0]
+st.write('333333:', dede)
 # Transformation en dataframe
 D1 = pd.DataFrame(D1)
 
